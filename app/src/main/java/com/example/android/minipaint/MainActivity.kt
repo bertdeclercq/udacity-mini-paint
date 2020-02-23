6e1cc548313e51b1e50d3f41b2b9b4a61755e89b
Bert De Clercq <xunobulax@gmail.com>
2020-02-23 15:50:02 +0100

    Finished project

--- app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         val myCanvasView = MyCanvasView(this)
         myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
