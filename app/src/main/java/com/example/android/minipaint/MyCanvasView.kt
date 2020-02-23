4105ee9d602e31324a430054b594c278897e6297
Bert De Clercq <xunobulax@gmail.com>
2020-02-23 20:52:38 +0100

    Save drawings in a path instead of a bitmap

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -1,7 +1,10 @@
 package com.example.android.minipaint
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -13,8 +16,11 @@ private const val STROKE_WIDTH = 12f
 
 class MyCanvasView(context: Context) : View(context) {
 
-    private lateinit var extraCanvas: Canvas
-    private lateinit var extraBitmap: Bitmap
+//    private lateinit var extraCanvas: Canvas
+//    private lateinit var extraBitmap: Bitmap
+
+    private val drawing = Path()
+    private val curPath = Path()
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
 
@@ -30,7 +36,7 @@ class MyCanvasView(context: Context) : View(context) {
         strokeWidth = STROKE_WIDTH
     }
 
-    private var path = Path()
+//    private var path = Path()
 
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
@@ -45,10 +51,10 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        if (::extraBitmap.isInitialized) extraBitmap.recycle()
-        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)
+//        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+//        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+//        extraCanvas = Canvas(extraBitmap)
+//        extraCanvas.drawColor(backgroundColor)
         val inset = 40
         frame = Rect(inset, inset, w - inset, h - inset)
     }
@@ -56,7 +62,9 @@ class MyCanvasView(context: Context) : View(context) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        canvas.drawColor(backgroundColor)
+        canvas.drawPath(drawing, paint)
+        canvas.drawPath(curPath, paint)
         canvas.drawRect(frame, paint)
     }
 
@@ -74,14 +82,16 @@ class MyCanvasView(context: Context) : View(context) {
     }
 
     private fun touchUp() {
-        path.reset()
+        drawing.addPath(curPath)
+        curPath.reset()
+//        path.reset()
     }
 
     private fun touchMove() {
         val dx = abs(motionTouchEventX - currentX)
         val dy = abs(motionTouchEventY - currentY)
         if (dx >= touchTolerance || dy >= touchTolerance) {
-            path.quadTo(
+            curPath.quadTo(
                 currentX,
                 currentY,
                 (motionTouchEventX + currentX) / 2,
@@ -89,14 +99,14 @@ class MyCanvasView(context: Context) : View(context) {
             )
             currentX = motionTouchEventX
             currentY = motionTouchEventY
-            extraCanvas.drawPath(path, paint)
+//            extraCanvas.drawPath(path, paint)
         }
         invalidate()
     }
 
     private fun touchStart() {
-        path.reset()
-        path.moveTo(motionTouchEventX, motionTouchEventY)
+        curPath.reset()
+        curPath.moveTo(motionTouchEventX, motionTouchEventY)
         currentX = motionTouchEventX
         currentY = motionTouchEventY
     }
